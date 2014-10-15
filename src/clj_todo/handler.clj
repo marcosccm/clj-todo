dd9ebe318ece06e23b29db24a1f01fc681403cde
Marcos Matos <marcosccm@gmail.com>
2014-10-15 18:13:29 -0300

    Now with post!

--- src/clj_todo/handler.clj
+++ src/clj_todo/handler.clj
@@ -1,6 +1,9 @@
 (ns clj-todo.handler
   (:require [compojure.core :refer :all]
-            [compojure.route :as route])
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [hiccup.form :refer :all]
+            [hiccup.middleware :refer [wrap-base-url]])
   (:use [hiccup.core]))
 
 (def todos
@@ -12,9 +15,24 @@
   []
   (html
     [:body
-     [:h1 "Todo List"]
-     [:ul
-      (map (fn [todo] [:li todo]) @todos)]]))
+      [:h1 "Todo List"]
+      [:ul
+        (map (fn [todo] [:li todo]) @todos)]
+      [:h1 "New Todo"]
+      [:form { :action "/" :method "post"}
+        [:label { :for "todo" } "Todo"]
+        (text-field "todo")
+        (submit-button "submit")]]))
 
-(defroutes app
-  (GET "/" [] (todo-list)))
+(defn add-todo
+  [todo]
+  (dosync (alter todos conj todo)))
+
+(defroutes app-routes
+  (GET "/" [] (todo-list))
+  (POST "/" [todo] (do (add-todo todo) (todo-list))))
+
+(def app
+  (-> (routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
