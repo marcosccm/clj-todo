15223398a1055e3e6499dee86fc4562d78e48613
Marcos Matos <marcosccm@gmail.com>
2014-10-15 15:36:32 -0300

    Display todos! such beauty!

--- src/clj_todo/handler.clj
+++ src/clj_todo/handler.clj
@@ -3,11 +3,18 @@
             [compojure.route :as route])
   (:use [hiccup.core]))
 
+(def todos
+  ["Buy some milk"
+   "Wash the dishes"
+   "conquer the world"])
+
 (defn todo-list
   []
   (html
     [:body
-     [:h1 "Potato"]]))
+     [:h1 "Todo List"]
+     [:ul
+      (map (fn [todo] [:li todo]) todos)]]))
 
 (defroutes app
   (GET "/" [] (todo-list)))
