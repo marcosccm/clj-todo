b7ec2a8af116d2d44b63b3a633ed15e919e54a34
Marcos Matos <marcosccm@gmail.com>
2014-10-26 18:24:41 -0200

    Separate layout from views

--- src/clj_todo/handler.clj
+++ src/clj_todo/handler.clj
@@ -8,21 +8,27 @@
 
 (def todos
   (ref ["Buy some milk"
-   "Wash the dishes"
-   "conquer the world"]))
+        "Wash the dishes"
+        "conquer the world"]))
+
+(defn layout
+  [& content]
+  (html
+    [:head
+      [:title "Clj Todo!"]]
+    [:body content]))
 
 (defn todo-list
   []
-  (html
-    [:body
-      [:h1 "Todo List"]
-      [:ul
-        (map (fn [todo] [:li todo]) @todos)]
-      [:h1 "New Todo"]
-      [:form { :action "/" :method "post"}
-        [:label { :for "todo" } "Todo"]
-        (text-field "todo")
-        (submit-button "submit")]]))
+  (layout
+    [:h1 "Todo List"]
+    [:ul
+      (map (fn [todo] [:li todo]) @todos)]
+    [:h1 "New Todo"]
+    [:form { :action "/" :method "post"}
+      [:label { :for "todo" } "Todo"]
+      (text-field "todo")
+      (submit-button "submit")]))
 
 (defn add-todo
   [todo]
